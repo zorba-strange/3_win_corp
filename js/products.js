14dfd74f5e8b33363c7bf8999256233f77489ffe
zorba-strange <winston.davis.mclean@gmail.com>
2017-07-09 12:11:00 -0400

    working on products page

---
+++ js/products.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    console.log('hello products');
+    // $('.product_sliders').slick({
+    //     autoplay: true,
+    //     dots: true,
+    //     speed: 3000,
+    //     infinite: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    // });
+});
